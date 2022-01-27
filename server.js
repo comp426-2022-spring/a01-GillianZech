ebded21f21411b45f0ceccf26d44178ae56cbfe1
Gillian Zech <ginger01@email.unc.edu>
2022-01-26 22:50:14 -0500

    Added browser-sync

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ fs.readFile('./www/index.html', 'utf8' , (err, data) => {
 // 3. end with the data that you are reading in from ./www/index.html.
 
     const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello, World!</h1>')
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/html')
+      res.end('<h1>Hello, World!</h1>')
     })
 
 
